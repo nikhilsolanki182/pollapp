9868d4b504c0ba6ab88f659aa0fcf882672254bc
user1 <nikhilsolanki8202@gmail.com>
2026-01-23 12:32:46 +0530

    Updated polls views implement render

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,9 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
-
-
-# Leave the rest of the views (detail, results, vote) unchanged
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
