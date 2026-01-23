d851e4fe385942c8e65a0f38c36278fc468f90ef
user1 <nikhilsolanki8202@gmail.com>
2026-01-23 12:35:50 +0530

    Updated polls views last done

--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,9 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Question
 
 
 # ...
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
